7e0087252b98245bcb58d69df9d130091890d931
ErrorThree <80226295+ErrorThree@users.noreply.github.com>
2023-05-19 14:25:08 +0000

    e

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+const form = document.getElementById('converter-form');
+const resultDiv = document.getElementById('result');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  let integer = parseInt(document.getElementById('integer').value);
+  let Minimum = parseInt(document.getElementById('Minimum').value);
+  let Maximnum = parseInt(document.getElementById('Maximnum').value);
+  let string = "";
+  let number = integer;
+  function multiplicationtable() {
+    for (let counting = Minimum; counting <= Maximnum; counting++) {
+      number = integer*counting
+      string = string + `${integer} x ${counting} = ${number} <br>`
+    }
+    return string
+  }
+  resultDiv.innerHTML = `${multiplicationtable()}`
+});
